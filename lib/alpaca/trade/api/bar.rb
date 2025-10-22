3e7a58f5bc95cc6c06c5467c34d19f724643fb61
Eric <eric@unfoundedlabs.com>
2025-10-22 13:43:11 -0400

    trades use fractional seconds

--- lib/alpaca/trade/api/bar.rb
+++ lib/alpaca/trade/api/bar.rb
@@ -5,7 +5,7 @@ module Alpaca
     module Api
       class Bar
         attr_reader :time, :open, :high, :low, :close, :volume
-
+        # FYI alpaca docs as of 10-22-2025 seem incorrect. bars have second level, not fractional seconds
         def initialize(json)
           format = "%Y-%m-%dT%H:%M:%SZ"
           @time = DateTime.strptime(json['t'], format)
--- lib/alpaca/trade/api/trade.rb
+++ lib/alpaca/trade/api/trade.rb
@@ -6,6 +6,7 @@ module Alpaca
 
         def initialize(json)
           format = "%Y-%m-%dT%H:%M:%SZ"
+          format = "%Y-%m-%dT%H:%M:%S.%NZ"
           @time = DateTime.strptime(json['t'], format)
           @size = json['s']
           @conditions = json['c']
